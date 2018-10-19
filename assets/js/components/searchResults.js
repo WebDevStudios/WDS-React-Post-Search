b3ddc30b566c4d40010cdd73ab142f568be34737
Corey Collins <coreymcollins@gmail.com>
2018-10-19 11:28:41 -0400

    Adjusts functionality for when to show the loading text

--- assets/js/components/searchResults.js
+++ assets/js/components/searchResults.js
@@ -12,20 +12,14 @@ export default class SearchResults extends React.Component {
 		let results      = '',
 			resultsClass = '';
 
+		// If the input is empty, like we deleted our query.
 		if ( this.props.empty ) {
 			return null;
-		} else if ( this.props.loading ) {
+		}
 
-			results      = <p>{ wds_react_post_search.loading_text }</p>;
-			resultsClass = ' loading performed-action';
+		if ( 0 < this.props.results.length || 0 < this.props.results.length && this.props.loading ) {
 
-		} else if ( ! this.props.empty && this.props.lengthError ) {
-
-			results      = <p>{ wds_react_post_search.length_error }</p>;
-			resultsClass = ' character-length performed-action';
-
-		} else if ( 0 < this.props.results.length ) {
-
+			// If we have results, OR we have results, but we're still typing. We don't want to take the results away to load more.
 			const queryResults = this.props.results.map( result => {
 				return(
 					<SearchResult key={ result.id } result={ result } />
@@ -35,13 +29,27 @@ export default class SearchResults extends React.Component {
 			results      = <ul className="search-results-list">{ queryResults }</ul>;
 			resultsClass = ' has-results performed-action';
 
+		} else if ( this.props.loading ) {
+
+			// If we're loading results for the first time.
+			results      = <p>{ wds_react_post_search.loading_text }</p>;
+			resultsClass = ' loading performed-action';
+
+		} else if ( ! this.props.empty && this.props.lengthError ) {
+
+			// If the input isn't empty but doesn't have enough characters.
+			results      = <p>{ wds_react_post_search.length_error }</p>;
+			resultsClass = ' character-length performed-action';
+
 		} else if ( this.props.searched ) {
 
+			// If no results were found.
 			results      = <p>{ wds_react_post_search.no_results_text }</p>;
 			resultsClass = ' no-results performed-action';
 
 		}
 
+		// Bail if we have no results variables set.
 		if ( ! results ) {
 			return null;
 		}
