653cf2de69512da7ed18f005bbbcb5a59d79823d
Dave Bonds <db@davebonds.com>
2018-11-19 11:09:53 -0800

    Version bump

--- class-wds-react-post-search.php
+++ class-wds-react-post-search.php
@@ -3,7 +3,7 @@
  * Plugin Name: WDS React Post Search
  * Description: Power up the basic WordPress search with React.
  * Plugin URI: https://www.webdevstudios.com
- * Version: 1.0.0
+ * Version: 1.0.2
  *
  * With help from:
  * https://www.ibenic.com/wordpress-react-search/
@@ -29,7 +29,7 @@ final class WDS_React_Post_Search {
 	 * @var    string
 	 * @since  0.0.0
 	 */
-	const VERSION = '1.0.0';
+	const VERSION = '1.0.2';
 
 	/**
 	 * URL of plugin directory.
